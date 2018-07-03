b228d115636e5fa00ed69c0e2a5a0213d45f5cb1
DunnCreativeSS <jarett+github@dcssquared.com>
2018-07-03 18:10:48 -0300

    lala

--- trades.js
+++ trades.js
@@ -170,7 +170,7 @@ app.get('/', function (req, res){
 	if (count > 0 && (poloDiff > 1.01 || poloDiff < 0.99)){
 		msg+=(prices[pairs[p].split('_')[1] + pairs[p].split('_')[0]]);
 		msg+='<br>'+ (count + ' count');
-		msg+=(prices[pairs[p].split('_')[1] + pairs[p].split('_')[0]]).toString();
+		msg+='<br>' + (prices[pairs[p].split('_')[1] + pairs[p].split('_')[0]]).toString();
 	msg+='<br>'+(pairs[p].split('_')[1] + pairs[p].split('_')[0] + ' avg ' + avg);
 	msg+=('<br>polo rate: ' + prices[pairs[p].split('_')[1] + pairs[p].split('_')[0]]['poloniex']);
 	msg+=('<br>poloDiff: ' + poloDiff);
